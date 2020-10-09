ac0b00a13385394d4ce5145e3d5336f403028480
DRAGON0818 <819698897@qq.com>
2020-10-09 21:35:06 +0800

    update

--- src/Chapter01Stream/DownstreamCollectors.java
+++ src/Chapter01Stream/DownstreamCollectors.java
@@ -4,16 +4,14 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Stream;
+import java.util.stream.*;
 import static java.util.stream.Collectors.*;
 
 public class DownStreamCollectors {
-
     public static class City{
         private String name;
         private String state;
         private int population;
-
 
         public City(String name, String state, int population) {
             this.name = name;
@@ -21,21 +19,35 @@ public class DownStreamCollectors {
             this.population = population;
         }
 
+
         public String getName() {
             return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
         }
 
         public String getState() {
             return state;
         }
 
+        public void setState(String state) {
+            this.state = state;
+        }
+
         public int getPopulation() {
             return population;
+        }
+
+        public void setPopulation(int population) {
+            this.population = population;
         }
     }
 
     public static Stream<City> readCities(String filename) throws IOException {
-        return Files.lines(Paths.get(filename)).map(l -> l.split(",")).map(a -> new City(a[0], a[1], Integer.parseInt(a[2])));
+        return Files.lines(Paths.get(filename)).map(l -> l.split(",")).
+                map(a->new City(a[0],a[1],Integer.parseInt(a[2])));
     }
 
     public static void main(String[] args) throws IOException {
@@ -47,23 +59,33 @@ public class DownStreamCollectors {
         Map<String, Long> countryToLocaleCounts = locales.collect(groupingBy(Locale::getCountry, counting()));
         System.out.println("countryToLocaleCounts:" + countryToLocaleCounts);
 
-        Stream<City> cities = readCities("E:\\IDEAworkspace\\JavaMain2\\cities.txt");
-        Map<String, Integer> stateToCityPopulation = cities.collect(groupingBy(City::getName, summingInt(City::getPopulation)));
-        System.out.println("stateToCityPopulation:"+stateToCityPopulation);
+        Stream<City> cities = readCities("/Users/donglixin/Downloads/corejava/v2ch01/cities.txt");
+        Map<String, Optional<City>> stateLargestCity = cities.collect(
+                groupingBy(City::getState, maxBy(Comparator.comparing(City::getPopulation)))
+        );
+        stateLargestCity.forEach((x,y)->{
+            System.out.println(x+" "+y.get().getName());
+        });
 
-        cities = readCities("E:\\IDEAworkspace\\JavaMain2\\cities.txt");
-        Map<String, Optional<String>> stateToLongestCityName = cities.collect(groupingBy(City::getState,
-                mapping(City::getName, maxBy(Comparator.comparing(String::length)))));
-        System.out.println("stateToLongestCityName: " + stateToLongestCityName);
+        cities = readCities("/Users/donglixin/Downloads/corejava/v2ch01/cities.txt");
+        Map<String, Optional<String>> stateToLongestCityName = cities.collect(groupingBy(
+                City::getState, mapping(City::getName, maxBy(Comparator.comparing(String::length)))
+        ));
+        System.out.println("@@@@@"+stateToLongestCityName);
 
-        System.out.println("************************************************");
+        Stream<Locale> l = Stream.of(Locale.getAvailableLocales());
+        Map<String, Set<String>> countryToLanguages = l.collect(
+                groupingBy(Locale::getDisplayCountry,
+                        mapping(Locale::getDisplayLanguage, toSet()))
+        );
+        System.out.println("在"+countryToLanguages);
 
-        locales = Stream.of(Locale.getAvailableLocales());
-        Map<String, Set<String>> countryToLanguages = locales.collect(groupingBy(Locale::getDisplayCountry,
-                mapping(Locale::getDisplayLanguage, toSet())));
-        System.out.println("countryToLanguages:" + countryToLanguages);
-        System.out.println("\n");
+        cities = readCities("/Users/donglixin/Downloads/corejava/v2ch01/cities.txt");
+        Map<String, String> stateToCityName = cities.collect(groupingBy(
+                City::getState,
+                reducing("", City::getName, (s, t) -> s.length() == 0 ? t : s + "," + t)
+        ));
+        System.out.println("stateToCityNames:"+stateToCityName);
 
     }
-
 }
